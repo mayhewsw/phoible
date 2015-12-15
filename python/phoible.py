5c1425aa6c9e674429f53e8289ae16e7765a8c5e
Stephen Mayhew <swm.mayhew@gmail.com>
2015-12-15 16:59:58 -0500

    Updated modes on nearly every file... oops.

--- python/phoible.py
+++ python/phoible.py
@@ -23,7 +23,7 @@ class Phoneme:
         return self.GlyphID.__hash__()
 
 
-def getHRLanguages(fname, hrthreshold=1000):
+def getHRLanguages(fname, hrthreshold=0):
     """
     :param fname: the name of the file containing filesizes. Created using wc -l in the wikidata folder
     :param hrthreshold: how big a set of transliteration pairs needs to be considered high resource
@@ -56,10 +56,10 @@ def loadLangs(fname):
             inventoryid = sline[0]
             langcode = sline[2]
 
-            code2name[langcode] = sline[3]
-
             if sline[4] != "1":
                 continue
+            
+            code2name[langcode] = sline[3]
 
             p = Phoneme(*sline[-6:])
 
@@ -67,13 +67,24 @@ def loadLangs(fname):
             langs[langcode].add(p)
             
     return langs,code2name
-    
-    
+
+def loadLangData(fname):
+    with open(fname) as p:
+        lines = p.readlines()
+        header = lines[0].split("\t")
+        outdct = {}
+        for line in lines[1:]:
+            sline = line.split("\t")
+            code = sline[2]
+            ldict = dict(zip(header, sline))
+            outdct[code] = ldict
+
+    return outdct
+                
 
 def langsim(query, langs, only_hr=False):
 
     orig = langs[query]
-
 
     import os
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -85,18 +96,43 @@ def langsim(query, langs, only_hr=False):
         if langid == query:
             continue
         if not only_hr or langid in hrlangs:
-            d = len(langs[langid].intersection(orig))
-            dists.append((d, langid))
+            # try getting F1 here instead of just intersection.
+            #d = len(langs[langid].intersection(orig))
+            tgt = langs[langid]
+            tp = len(tgt.intersection(orig))
+            fp = len(tgt.difference(orig))
+            fn = len(orig.difference(tgt))
+            prec = tp / float(tp + fp) # this is also len(tgt)
+            recall = tp / float(tp + fn) # this is also len(orig)
+            f1 = 2 * prec * recall / (prec + recall)
+            dists.append((f1, langid))
             
-    topk = 100
+    topk = 500
     ret = sorted(dists, reverse=True)[:topk]
 
     return ret
 
+def compare(lang1, lang2, langs):
+    print "comparing", lang1, lang2
+
+    src = langs[lang1]
+    tgt = langs[lang2]
+    tp = len(tgt.intersection(src))
+    fp = len(tgt.difference(src))
+    fn = len(src.difference(tgt))
+    prec = tp / float(tp + fp) # this is also len(tgt)
+    recall = tp / float(tp + fn) # this is also len(orig)
+    f1 = 2 * prec * recall / (prec + recall)
+    return f1
+
+    
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("lang")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--langsim", nargs = 1)
+    group.add_argument("--langdata", nargs = 1)
+    group.add_argument("--compare", nargs = 2)
     #parser.add_argument("threshold", type=float)
     
     #parser.add_argument("--topk", help="show top k results", type=int, default=10)
@@ -104,10 +140,18 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
 
-    print "lang: ", args.lang
     #print "threshold: ", args.threshold
 
     #print langsim("language.csv", args.lang, args.threshold, phon=args.phon, topk=args.topk, only_hr=args.highresource)
-
-    langs,code2name = loadLangs("../gold-standard/phoible-phonemes.tsv")
-    print langsim(args.lang, langs, only_hr=args.highresource)
+    if args.langsim:
+        print "lang: ", args.langsim
+        langs,code2name = loadLangs("../gold-standard/phoible-phonemes.tsv")
+        print langsim(args.langsim[0], langs, only_hr=args.highresource)
+    elif args.compare:
+        print "langs: ", args.compare
+        langs,code2name = loadLangs("../gold-standard/phoible-phonemes.tsv")
+        print compare(args.compare[0], args.compare[1], langs)
+    elif args.langdata:
+        print "getting langdata... for", args.langdata
+        d = loadLangData("../gold-standard/phoible-aggregated.tsv")
+        print d[args.langdata[0]]
